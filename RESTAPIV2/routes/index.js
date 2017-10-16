02e1ebf6c86e1faaf1ee589527fafa7c108df7a3
Arthur Dayton <adayton@vlamis.com>
2017-10-16 11:31:06 -0500

    This is the unbroken one

--- RESTAPIV2/routes/index.js
+++ RESTAPIV2/routes/index.js
@@ -2,7 +2,15 @@
 /*
  * GET home page.
  */
+var path = require('path')
+, file = path.join(path.resolve('../', '/../', '../'),'Users','arthur','Documents','GIT','D3Example','EisenhowerPassOACLinux.html');
 
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+exports.index2 = function(req, res){
+	res.sendfile(file);
+	};
+
+
