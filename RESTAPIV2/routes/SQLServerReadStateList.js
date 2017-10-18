4fb27404b5e88cdfd82ffc35c147776c89dd76af
Arthur Dayton <adayton@vlamis.com>
2017-10-18 07:22:48 -0500

    Added CheckState webservice See ReadMe in RestAPIV2

--- RESTAPIV2/routes/SQLServerReadStateList.js
+++ RESTAPIV2/routes/SQLServerReadStateList.js
@@ -1,6 +1,6 @@
 //
 
-//var Request = require('tedious').Request;
+var Request = require('tedious').Request;
 var TYPES = require('tedious').TYPES;
 
 //custom module so we don't have to rewrite connection every time
@@ -15,6 +15,7 @@ SQLConnect=require('../tools/SQLServerCommon');
 //Exports modules for route
 exports.getListofStateID = function(req, res){commonFetch(req, res,'getListofStateID')};
 exports.getObjectString = function(req, res){commonFetch(req, res,'getObjectString')};	 
+exports.checkExistence = function(req, res){commonFetch(req, res,'checkExistence')};	
 	
 //function for fetching data
 //takes route parameter to decide what sql to submit
@@ -46,7 +47,9 @@ exports.getObjectString = function(req, res){commonFetch(req, res,'getObjectStri
 	      else if(route=='getObjectString'){
 	    	  getObject();
 	      }
-	      
+	      else if(route=='checkExistence'){
+	    	 checkState();
+	      }
 	    }
 	  });
 	  
@@ -70,10 +73,6 @@ exports.getObjectString = function(req, res){commonFetch(req, res,'getObjectStri
 		    	//process rows returned from sql statement
 		        request.on('row', function(columns) {
 			          columns.forEach(function(column) {
-			        	  
-			           // console.log(column.metadata.colName+' : '+column.value);
-			           // console.log('type: '+column.metadata.type.name);
-			          //  console.log('typechange6: '+column.metadata.dataLength); 
 			            
 			            if(column.metadata.colName=='USERNAME'){
 			            	respUser=column.value;
@@ -125,14 +124,8 @@ exports.getObjectString = function(req, res){commonFetch(req, res,'getObjectStri
 	    	
 	        request.on('row', function(columns) {
 		          columns.forEach(function(column) {
-		        	  
-		           // console.log(column.metadata.colName+' : '+column.value);
-		          //  console.log('type: '+column.metadata.type.name);
-		          //  console.log('typechange6: '+column.metadata.dataLength);
 		            
 		            if(column.metadata.colName=='OBJECTSTRING'){
-		          //  objnew=JSON.parse(column.value)
-		           // console.log('val: '+objnew.value1)
 		            
 		             SQLresponse.results.push({"OBJECTSTRING":column.value})
 		          //  return res.send(JSON.stringify(objnew))
@@ -151,6 +144,70 @@ exports.getObjectString = function(req, res){commonFetch(req, res,'getObjectStri
 	       
 	    	}
 	 }  
+
+	    function checkState() {
+	    	//if missing parameters then reject else continue
+	    	if(!req.query.USERNAME|| !req.query.STATEID||!req.query.PATH) {
+	    		return res.send({"status": "error", "message": "missing user, path or stateid"});
+	    	}
+	    	else{
+	    		
+	    		
+	    	var sqlstmt="SELECT 1 FROM [TESTINGNPMWRITEBACK].[dbo].[UP_SAVED_STATE] " +
+	    			"where [USERNAME]=@USER and [STATEID]=@STATEID and [PATH]=@PATH"
+	    	
+	    	//var request=SQLConnect.myRequest(sqlstmt,res);
+	    	
+		    var request= new Request(sqlstmt, function(err,rowCount) {
+			    if (err) {
+			      console.log(err);
+			    } else {
+			    	if(rowCount==0){
+			    		return res.send('{"exists":"false"}')
+			    	}
+			    	else if(rowCount==1){
+			    		return res.send('{"exists":"true"}')
+			    	}
+			    	else{
+			    		return res.send('{"exists":"unknown result - row count="'+rowCount+'}')
+			    	}
+		        	
+		        	
+		            var status= connection.close()
+		            console.log('status-'+status)
+			    }
+			  });
+	    	
+	    	request.addParameter('USER', TYPES.NVarChar, req.query.USERNAME);
+	    	request.addParameter('STATEID', TYPES.NVarChar, req.query.STATEID);
+	    	request.addParameter('PATH', TYPES.NVarChar, req.query.PATH);
+	    	
+	    	
+	       /* request.on('row', function(columns) {
+		          columns.forEach(function(column) {
+		            
+		            if(column.metadata.colName=='OBJECTSTRING'){
+		          //  objnew=JSON.parse(column.value)
+		           // console.log('val: '+objnew.value1)
+		            
+		             SQLresponse.results.push({"OBJECTSTRING":column.value})
+		          //  return res.send(JSON.stringify(objnew))
+
+		            }
+		            
+		          });
+	        });*/
+	        
+	        
+	        //handle sql execution complete event
+	      //  doneNess(request);
+	        
+	        //submit sql statment
+	        connection.execSql(request);
+	       
+	    	}
+	 }  
+	    
 	    
         function doneNess(request){
 	        request.on('done', function (rowCount, more, rows) {
